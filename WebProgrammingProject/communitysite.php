9a6842128a58ba0d8342603bd1667d975c39895a
hongry7210 <ssubong1215@naver.com>
2024-06-13 01:33:27 +0900

    최종

--- WebProgrammingProject/communitysite.php
+++ WebProgrammingProject/communitysite.php
@@ -112,6 +112,28 @@ mysqli_select_db($db, 'memberinfodb') or die(mysqli_error($db));
     <h1>Recent Post</h1>
 </div>
 <?php
+
+$db = mysqli_connect('localhost', 'root', '') or die ('Unable to connect. Check your connection parameters.');
+$query = 'CREATE DATABASE IF NOT EXISTS communitypost';
+
+mysqli_query($db, $query) or die(mysqli_error($db)); //db에 연결
+
+mysqli_select_db($db, 'memberinfodb') or die(mysqli_error($db)); //memberinfodb 데이터베이스를 선택
+
+$query = 'CREATE TABLE IF NOT EXISTS post (
+    postnum INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    posttitle VARCHAR(255) NOT NULL,
+    postcontent VARCHAR(255) NOT NULL,
+    gamename VARCHAR(255) NOT NULL,
+    postmemberid INTEGER NULL,
+    postmembername VARCHAR(255),
+    comment INTEGER UNSIGNED NOT NULL,
+    PRIMARY KEY (postnum)
+)
+ENGINE=MyISAM';
+
+mysqli_query($db, $query) or die(mysqli_error($db));
+
 $query = 'SELECT posttitle, postmembername, postnum, postcontent
 FROM post
 ORDER BY postnum DESC
--- WebProgrammingProject/post.php
+++ WebProgrammingProject/post.php
@@ -1,5 +1,23 @@
 <?php
 session_start();
+$db = mysqli_connect('localhost', 'root', '') or die ('Unable to connect. Check your connection parameters.');
+$query = 'CREATE DATABASE IF NOT EXISTS memberinfodb';
+
+mysqli_query($db, $query) or die(mysqli_error($db)); //db에 연결
+
+mysqli_select_db($db, 'memberinfodb') or die(mysqli_error($db)); //memberinfodb 데이터베이스를 선택
+
+$query = 'CREATE TABLE IF NOT EXISTS comment (
+    commentnum INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    commentcontent VARCHAR(255) NOT NULL,
+    commentmembername VARCHAR(255) NOT NULL,
+    postid INTEGER UNSIGNED NOT NULL,
+    PRIMARY KEY (commentnum)
+)
+ENGINE=MyISAM';
+
+mysqli_query($db, $query) or die(mysqli_error($db));
+
 $postid = $_GET['postnum'];
 $posttitle = $_GET['posttitle'];
 $postcontent = $_GET['postcontent'];
